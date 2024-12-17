70608927cea61c45e71235278874286ca90aa405
PrecutGhost <alexneo4513@gmail.com>
2024-12-17 14:35:29 +0800

    玩家动画2.0调整

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +12,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# 获取输入方向，方向可能为0、1、-1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# 判断移动方向并翻转玩家
+	if direction > 0 :
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# 设置玩家动画播放判断
+	#有限判断角色是否站在地面上，站在播放闲置和奔跑，否则播放跳跃
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+			
+	# 定义了移动动作
 	if direction:
 		velocity.x = direction * SPEED
 	else:
